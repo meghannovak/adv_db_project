b4d9b3862d26acd21d8b4131cd42b34413dd0be8
EC2 Default User <ec2-user@i-0e40b4ce21bcafd2b.aws.nd.edu>
2024-05-30 14:45:02 +0000

    Classes

---
+++ classes.py
@@ -0,0 +1,6 @@
+class User():
+    def __init__(self, username, password, first_name='', last_name=''):
+        self.username = username
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
